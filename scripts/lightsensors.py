c95edb5c68e4f56f2585a008ea7039fa5289365a
Besshy <toshiki-yasuda0113@keio.jp>
2019-09-01 11:24:34 +0000

    Make node ofreading /dev/rtlightsensor0

--- scripts/lightsensors.py
+++ scripts/lightsensors.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 import rospy
 
+
 if __name__ == "__main__":
     rospy.init_node("lightsensors")
-    rospy.spin()
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        with open("/dev/rtlightsensor0","r") as r:
+            l = r.readlines()
+            rospy.loginfo(l)
+            rate.sleep()
+            
