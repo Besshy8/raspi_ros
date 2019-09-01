d588d4e069dae481dd7df31bcc734cebd85db86a
Besshy <toshiki-yasuda0113@keio.jp>
2019-09-01 13:04:23 +0000

    Add abiity of publisher

--- scripts/lightsensors.py
+++ scripts/lightsensors.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 #encoding: utf8
 import rospy
-
+from raspi_ros.msg import LightSensors
 
 if __name__ == "__main__":
     rospy.init_node("lightsensors")
+    pub = rospy.Publisher("lightsensor_val",LightSensors,queue_size=10)
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
         with open("/dev/rtlightsensor0","r") as r:
@@ -14,4 +15,10 @@ if __name__ == "__main__":
             ##print(spl)
             d = [int(i) for i in spl]  ##文字列を整数にキャスト
             print("d[0]:%d,d[1]:%d,d[2]:%d,d[3]:%d" % (d[0],d[1],d[2],d[3]))
+            m = LightSensors()
+            m.r_side = d[0]
+            m.r_front = d[1]
+            m.l_front = d[2]
+            m.l_side = d[3]
+            pub.publish(m)
             rate.sleep()
