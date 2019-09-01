313626b2252f9b15e91fdcf03ad858a4f65b8934
Besshy <toshiki-yasuda0113@keio.jp>
2019-09-01 12:07:07 +0000

    Make sensor value list

--- scripts/lightsensors.py
+++ scripts/lightsensors.py
@@ -9,5 +9,10 @@ if __name__ == "__main__":
     while not rospy.is_shutdown():
         with open("/dev/rtlightsensor0","r") as r:
             l = [s.rstrip() for s in r.readlines()] ##改行が含まれてしまうので取り除いた
-            
+            for i in l:
+                spl = i.split(" ")
+            print(spl)
+            ##d = [int(i) for i in l]  ##文字列を整数にキャスト
+            ##rospy.loginfo("d[0]:%d",d[0])
+            #rospy.loginfo("l[0]:%s",l[0])
             rate.sleep()
