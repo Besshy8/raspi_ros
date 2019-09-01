2aa143f630dc7f0eba549e541668879f83f987c8
Besshy <toshiki-yasuda0113@keio.jp>
2019-09-01 12:17:48 +0000

    Cast str to Int

--- scripts/lightsensors.py
+++ scripts/lightsensors.py
@@ -10,9 +10,8 @@ if __name__ == "__main__":
         with open("/dev/rtlightsensor0","r") as r:
             l = [s.rstrip() for s in r.readlines()] ##改行が含まれてしまうので取り除いた
             for i in l:
-                spl = i.split(" ")
-            print(spl)
-            ##d = [int(i) for i in l]  ##文字列を整数にキャスト
-            ##rospy.loginfo("d[0]:%d",d[0])
-            #rospy.loginfo("l[0]:%s",l[0])
+                spl = i.split(" ")  ##spaceで要素を区切った
+            ##print(spl)
+            d = [int(i) for i in spl]  ##文字列を整数にキャスト
+            print("d[0]:%d,d[1]:%d,d[2]:%d,d[3]:%d" % (d[0],d[1],d[2],d[3]))
             rate.sleep()
