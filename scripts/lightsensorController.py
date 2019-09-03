a480867e2b1d4503bb517e77ef0a1339cad3b804
Besshy <toshiki-yasuda0113@keio.jp>
2019-09-03 10:55:05 +0000

    Add subscriber ability

--- scripts/lightsensorController.py
+++ scripts/lightsensorController.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 #encoding: utf8
 import rospy
+from raspi_ros.msg import LightSensors
 
+def getLightsensor(msg):
+    print("--------LightSensorsVal---------")
+    print("Right_side : %d" % msg.r_side)
+    print("Right_front: %d" % msg.r_front)
+    print("Left_front : %d" % msg.l_front)
+    print("Left_side  : %d" % msg.l_side)
+    
 if __name__ == "__main__":
     rospy.init_node("LightSensorController")
+    sub = rospy.Subscriber("lightsensor_val",LightSensors,getLightsensor)
     rospy.spin()
