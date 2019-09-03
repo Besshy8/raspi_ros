6ab1d59bdc3f7edc622c7381754c6cd7fdd53486
Besshy <toshiki-yasuda0113@keio.jp>
2019-09-03 11:37:06 +0000

    Add cmd_vel Publisher node

--- scripts/lightsensorController.py
+++ scripts/lightsensorController.py
@@ -2,6 +2,7 @@
 #encoding: utf8
 import rospy
 from raspi_ros.msg import LightSensors
+from geometry_msgs.msg import Twist
 
 def getLightsensor(msg):
     print("--------LightSensorsVal---------")
@@ -11,8 +12,18 @@ def getLightsensor(msg):
     print("Left_side  : %d" % msg.l_side)
     print("Sum        : %d" % msg.sum)
     print("Sum_forward: %d" % msg.sum_forward)
-    
+
+    m = Twist()
+    if msg.sum_forward >= 100:
+        m.linear.x = 0
+        m.angular.z = 0
+        pub.publish(m)     ## エラーは起こらないがコードの補完はない
+    else:
+        m.linear.x = 0.125
+        pub.publish(m)
+
 if __name__ == "__main__":
     rospy.init_node("LightSensorController")
+    pub = rospy.Publisher("motorCmdvel",Twist,queue_size=10)
     sub = rospy.Subscriber("lightsensor_val",LightSensors,getLightsensor)
     rospy.spin()
