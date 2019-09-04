10723a35b1f57f0a9b40efe1e082c61af17db19e
Besshy <toshiki-yasuda0113@keio.jp>
2019-09-04 07:19:13 +0000

    Add ability of keyboard input

--- scripts/keyboardController.py
+++ scripts/keyboardController.py
@@ -2,7 +2,16 @@
 #encoding: utf8
 
 import rospy 
+from geometry_msgs.msg import Twist
 
 if __name__ == "__main__":
     rospy.init_node("KeyBoardController")
+    pub = rospy.Publisher("motorCmdvel",Twist,queue_size=10)
+    print("----Enter key------")
+    while True:
+        val = raw_input("Enter key : ")
+        print("keyboard input is : %s" % val)
+        if val == "d":
+            print("keyboard input is shutdown")
+            break
     rospy.spin()
