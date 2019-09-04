f704ec685445fa2deb5dee8872e74c964b3236f3
Besshy <toshiki-yasuda0113@keio.jp>
2019-09-04 13:57:54 +0000

    Add ability of service

--- scripts/joystickController.py
+++ scripts/joystickController.py
@@ -4,10 +4,31 @@ import rospy
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist 
 import math
+from std_srvs.srv import Trigger
 
 def joyToCmdvel(msg):
     m = Twist()
-    if msg.buttons[0] == 1:
+    if msg.buttons[4] == 1:
+        rospy.wait_for_service("motor_on")
+        try:
+            srv = rospy.ServiceProxy("motor_on",Trigger)
+            on = srv()
+            print("---------------------")
+            print(on.success)
+            print("[message]: %s" % on.message)
+        except rospy.ServiceException as e:
+            print("can't get service : %s" % e)
+    elif msg.buttons[5] == 1:
+        rospy.wait_for_service("motor_off")
+        try:
+            srv = rospy.ServiceProxy("motor_off",Trigger)
+            off = srv()
+            print("---------------------")
+            print(off.success)
+            print("[message]: %s" % off.message)
+        except rospy.ServiceException as e:
+            print("can't get service : %s" % e)
+    elif msg.buttons[0] == 1:
         m.angular.z = math.pi / 2
         pub.publish(m)
     elif msg.buttons[1] == 1:
