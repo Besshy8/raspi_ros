f5ba9a2f1c4d586fafe5476c83642533ad9d2ce2
Besshy <toshiki-yasuda0113@keio.jp>
2019-09-04 08:18:16 +0000

    Add ability of change keyboard input to cmd_vel

--- scripts/keyboardController.py
+++ scripts/keyboardController.py
@@ -3,10 +3,14 @@
 
 import rospy 
 from geometry_msgs.msg import Twist
+import math
 
 if __name__ == "__main__":
     rospy.init_node("KeyBoardController")
     pub = rospy.Publisher("motorCmdvel",Twist,queue_size=10)
+    ##m = Twist()    ##ここにかくとwhileの中のmが更新されず累積する
+    ##rate = rospy.Rate(10)
+    '''
     print("----Enter key------")
     while True:
         val = raw_input("Enter key : ")
@@ -14,4 +18,30 @@ if __name__ == "__main__":
         if val == "d":
             print("keyboard input is shutdown")
             break
-    rospy.spin()
+        rospy.spin()    
+    '''
+    while not rospy.is_shutdown():
+        m = Twist()
+        val = raw_input("Enter key : ")
+        print("keyboard input is : %s" % val)
+        if val == "exit":
+            print("KeyBoard input exit")
+            break
+        if val == "w":
+            m.linear.x = 0.125  ##デバイスファイルには一度書き込まれると上書きするまで残るので動き続ける
+            pub.publish(m)
+        if val == "s":
+            m.linear.x = -0.125
+            pub.publish(m)
+        if val == "d":
+            m.angular.z = math.pi / 2
+            pub.publish(m)
+        if val == "a":
+            m.angular.z = - math.pi / 2
+            pub.publish(m)
+        if val == "q":
+            m.linear.x = 0.0
+            m.angular.z = 0.0
+            pub.publish(m)
+
+    
