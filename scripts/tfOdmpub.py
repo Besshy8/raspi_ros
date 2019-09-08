250be879976687ae5508f2cd58ad7497c32a7ac5
Besshy <toshiki-yasuda0113@keio.jp>
2019-09-08 01:19:25 +0000

    Add ability of subscriber motorCmdvel

--- scripts/tfOdmpub.py
+++ scripts/tfOdmpub.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
 #encoding: utf8
 import rospy
+from geometry_msgs.msg import Twist
+
+def transform_tfOdm(msg):
+    v_x = msg.linear.x
+    v_th = msg.angular.z
+    print("-----------")
+    print(v_x)
+    print(v_th)
+    return 0
 
 if __name__ == "__main__":
     rospy.init_node("Cmd_to_tfOdm")
+    sub = rospy.Subscriber("motorCmdvel",Twist,transform_tfOdm)
     rospy.spin()
