10b160161246ac673df1e0b0b151a515ae00f050
Besshy <toshiki-yasuda0113@keio.jp>
2019-09-08 01:31:04 +0000

    Define cmdvel_to_tf and cmdvel_to_Odm

--- scripts/tfOdmpub.py
+++ scripts/tfOdmpub.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python
 #encoding: utf8
-import rospy
+import rospy,tf,math
 from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
 
-def transform_tfOdm(msg):
+def cmdvel_to_tf():
+    return 0
+
+def cmdvel_to_Odm():
+    return 0
+
+
+def call_tfOdmtransformer(msg):
     v_x = msg.linear.x
     v_th = msg.angular.z
-    print("-----------")
-    print(v_x)
-    print(v_th)
+    print("==============")
+    print("velocity_x    : %f" % v_x)
+    print("velocity_theta: %f" % v_th)
+    cmdvel_to_tf(v_x,v_th)
+    cmdvel_to_Odm(v_x,v_th)
     return 0
 
 if __name__ == "__main__":
