64307c0af4ee5457e873eaf40524822bb98f178e
Besshy <toshiki-yasuda0113@keio.jp>
2019-10-07 08:07:04 +0000

    Make geometry_msgs/Twist publisher

--- scripts/detected_face.py
+++ scripts/detected_face.py
@@ -4,6 +4,8 @@ import rospy
 import cv2
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+import math
+from geometry_msgs.msg import Twist 
 
 def callback(msg):
     ##print("recieve image")
@@ -13,6 +15,8 @@ def callback(msg):
     except CvBridgeError as e:
         print(e)
     ##print(type(cv_image))
+    print(cv_image.shape)
+    print(cv_image.shape[1])
 
     cascade = cv2.CascadeClassifier("/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml")
     gray = cv2.cvtColor(cv_image,cv2.COLOR_RGB2GRAY)
@@ -23,6 +27,19 @@ def callback(msg):
     
     ##show_img_det = cv2.cvtColor(img_det, cv2.COLOR_BGR2RGB)
     ##print(type(show_img_det))
+
+    c = cv_image.shape[1] / 2
+    d = c - (x + w/2)
+    print(c)
+    print(d)
+
+    d_std = d / 320
+    p = d_std * (math.pi / 2)
+
+    m = Twist()
+    m.angular.z = p
+    pub_cmdvel.publish(m)
+
     try:
         pub.publish(bridge.cv2_to_imgmsg(img_det,"bgr8"))
     except CvBridgeError as e:
@@ -32,4 +49,5 @@ if __name__ == "__main__":
     rospy.init_node("RectangleFace")
     sub = rospy.Subscriber("cv_camera/image_raw",Image, callback)
     pub = rospy.Publisher("image",Image,queue_size=10)
+    pub_cmdvel = rospy.Publisher("motorCmdvel",Twist,queue_size=10)
     rospy.spin()
