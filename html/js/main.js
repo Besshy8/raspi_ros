9cee92e5aacdba39c5130348a42d9678911ecfe0
Besshy <toshiki-yasuda0113@keio.jp>
2019-10-09 08:16:18 +0000

    rename

--- html/js/main.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    $("#table-btn").click(function(){
-        $("#login-modal").fadeIn();
-    });
-
-    $("#submit-btn").click(function(){
-        $("#login-modal").fadeOut();
-    })
-});
