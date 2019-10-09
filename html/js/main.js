7934d32de0ad6597ecf4a963c740838799d6f41a
Besshy <toshiki-yasuda0113@keio.jp>
2019-10-09 08:11:51 +0000

    Clone html

---
+++ html/js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+    $("#table-btn").click(function(){
+        $("#login-modal").fadeIn();
+    });
+
+    $("#submit-btn").click(function(){
+        $("#login-modal").fadeOut();
+    })
+});
