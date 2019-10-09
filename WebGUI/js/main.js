d4b65b3a537f0969f0c6d7cdb4622122761522dd
Besshy <toshiki-yasuda0113@keio.jp>
2019-10-09 09:20:47 +0000

    Add camera controll

--- WebGUI/js/main.js
+++ WebGUI/js/main.js
@@ -5,5 +5,16 @@ $(function(){
 
     $("#submit-btn").click(function(){
         $("#login-modal").fadeOut();
-    })
+    });
+
+    //$("#camstream").attr('data', 'http://' + location.hostname + ':10000/stream?topic=/cv_camera/image_raw');
+    $("#camstream").attr('data', 'http://' + location.hostname + ':10000/stream?topic=/image');
+    /*
+    var ros = new ROSLIB.Ros({url: "ws://" + location.hostname + "9000"});
+
+    ros.on("connection", function(){console.log("websocket: connected");});
+    ros.on("error", function(error){console.log("websocket error", error);});
+    ros.on("close", function(){console.log("websocket: closed");});
+
+    */
 });
