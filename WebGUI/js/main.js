a8df4b240c4803506f75e19dfac3d6549ccf984e
Besshy <toshiki-yasuda0113@keio.jp>
2019-10-11 05:37:21 +0000

    Add sensor display

--- WebGUI/js/main.js
+++ WebGUI/js/main.js
@@ -7,14 +7,37 @@ $(function(){
         $("#login-modal").fadeOut();
     });
 
-    //$("#camstream").attr('data', 'http://' + location.hostname + ':10000/stream?topic=/cv_camera/image_raw');
     $("#camstream").attr('data', 'http://' + location.hostname + ':10000/stream?topic=/image');
-    /*
-    var ros = new ROSLIB.Ros({url: "ws://" + location.hostname + "9000"});
+    
+    var ros = new ROSLIB.Ros({url: "ws://" + location.hostname + ":9000"});
 
     ros.on("connection", function(){console.log("websocket: connected");});
     ros.on("error", function(error){console.log("websocket error", error);});
     ros.on("close", function(){console.log("websocket: closed");});
 
-    */
+    var ls = new ROSLIB.Topic({
+        ros : ros,
+        name : 'lightsensor_val',
+        messageType : 'raspi_ros/LightSensors',
+    })
+
+    ls.subscribe(message => {
+        $(".value-right-side-modal").text(message.r_side);
+        $(".value-right-front-modal").text(message.r_front);
+        $(".value-left-front-modal").text(message.l_front);
+        $(".value-left-side-modal").text(message.l_side);
+        $(".value-sum-all-modal").text(message.sum);
+        $(".value-sum-front-modal").text(message.sum_forward);
+
+        $(".value-right-side").text(message.r_side);
+        $(".value-right-front").text(message.r_front);
+        $(".value-left-front").text(message.l_front);
+        $(".value-left-side").text(message.l_side);
+        $(".value-sum-all").text(message.sum);
+        $(".value-sum-front").text(message.sum_forward);
+        
+        //console.log(message);
+
+        
+    });
 });
